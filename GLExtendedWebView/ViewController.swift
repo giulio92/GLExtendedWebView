9667cbde9dc64ce94fafca78dfd7597a0c3a62e5
Giulio Lombardo <giulio.lombardo@gmail.com>
2017-08-08 16:27:38 +0200

    Initial Commit

---
+++ GLExtendedWebView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GLExtendedWebView
+//
+//  Created by Lombardo Giulio on 08/08/17.
+//  Copyright © 2017 Giulio Lombardo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
